712b76eead6e802a1bd63dda782eed25cd9a5fcf
joehall87 <joehall87@gmail.com>
2018-12-13 23:23:30 +0000

    Created first bash at a home page using bootstrap

--- main.py
+++ main.py
@@ -10,14 +10,53 @@ app = Flask(__name__)
 RESOURCES_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources')
 
 
+class Book(object):
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+
+torah = [
+    Book('Genesis', 'gen'),
+    Book('Exodus', 'exo'),
+    Book('Leviticus', 'lev'),
+    Book('Numbers', 'num'),
+    Book('Deuteronomy', 'deu'),
+]
+
+neviim = [
+    Book('Joshua', 'jos'),
+    Book('Judges', 'jud'),
+    Book('Samuel', 'sam'),
+    Book('Kings', 'kin'),
+    Book('Isaiah', 'isa'),
+    Book('Jeremiah', 'jer'),
+    Book('Ezekial', 'eze'),
+    Book('The Twelve', 't12'),  # TODO Fix
+]
+
+ketuvim = [
+    Book('Psalms', 'psa'),
+    Book('Proverbs', 'pro'),
+    Book('Job', 'job'),
+    Book('Song of Songs', 'son'),
+    Book('Ruth', 'rut'),
+    Book('Lamentations', 'lam'),
+    Book('Ecclesiastes', 'ecc'),
+    Book('Esther', 'est'),
+    Book('Daniel', 'dan'),
+    Book('Ezra-Nehemiah', 'ezr'),
+    Book('Chronicles', 'chr'),
+]
+
+
 @app.route('/')
 def root():
-    """Show Tanach"""
-    gen = os.path.join(RESOURCES_DIR, 'Books', 'Genesis.acc.txt')
-    with open(gen, 'r') as f:
-        lines = (line.replace('\u202a', '').replace('\u202b', '').replace('\u202c', '').strip() for line in f.readlines())
-        verses = [_parse_line(line) for line in lines if not line.startswith('xxxx')]
-    return render_template('root.html', verses=verses)
+    """Show Tanakh"""
+    #gen = os.path.join(RESOURCES_DIR, 'Books', 'Genesis.acc.txt')
+    #with open(gen, 'r') as f:
+    #    lines = (line.replace('\u202a', '').replace('\u202b', '').replace('\u202c', '').strip() for line in f.readlines())
+    #    verses = [_parse_line(line) for line in lines if not line.startswith('xxxx')]
+    return render_template('home.html', torah=torah, neviim=neviim, ketuvim=ketuvim)
 
 
 def _parse_line(line):
